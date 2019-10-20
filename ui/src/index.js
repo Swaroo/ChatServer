d451348985996864b39c23696323ebad0bcb41f9
jose14520 <jose14520@gmail.com>
2019-10-19 23:24:19 -0700

    Added simple board UI

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,111 +1,24 @@
 import React from 'react'
-import { render } from 'react-dom'
-import axios from 'axios'; // axios seemed like a cool package to do api calls, alternative is fetch
+import ReactDOM from 'react-dom'
+import './index.css'
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 
+import App from './App'
+import Login from './login'
+import Board from './board'
+import Notfound from './notfound'
 
-class Login extends React.Component {
-  constructor(props){
-    super(props);
-
-    // data that user types in from login form
-    this.state = {
-      username: '',
-      password: '',
-      tokens: {
-        
-      }
-    };
-
-  }
-
-  copyState = (obj) => {
-    return Object.assign({}, obj);
-  }
-
-  // update username in state when keys are typed into login form
-  onNameChange = (event) =>{
-    this.setState({username: event.target.value})
-  }
-
-   // update password in state when keys are typed into login form
-  onPasswordChange = (event) =>{
-    this.setState({password: event.target.value})
-  }
-
-  // call post to server when user submits login form
-  onSubmit = (event) => {
-    event.preventDefault(); // prevents page from reloading with username + password visible as url query
-
-    // log that username is visible from our state
-    console.log("Login form submitted\n");
-    console.log("username: " + this.state.username);
-    console.log("password: " + this.state.password);
-
-   // call post with user data
-   axios.post( "http://localhost:3000/login", null, { 
-      params: {
-        username: this.state.username,
-        password: this.state.password 
-      }, 
-      headers: { "Access-Control-Allow-Origin": "*", } 
-    }).then((response) => {
-      console.log(response);
-
-      // get JWT Token
-      var token = response["data"]["token"]
-
-      // store token in state as {user: token}
-      var all_tokens = this.copyState(this.state.tokens);
-      all_tokens[this.state.username] = token
-      this.setState({tokens: all_tokens})
-      console.log(this.state.tokens);
-
-      //TODO: Redirect to messageboard
-
-    }, (error) => {
-      console.log(error);
-      if (error.response.status === 403){
-        console.log("403");
-        alert("Wrong password");
-      }
-
-    });
-
-
-    
-   /*
-   fetch('http://localhost:3000/login', {
-    method: 'POST',
-    body: 'username=aa&password=bb' 
-  }).then(function(response) {
-    console.log(response);
-  });
-  */
-  
-
-   
-	}
-	
-	render() {
-		return (
-			<div>
-				<form onSubmit={this.onSubmit}>
-          <h1>Login</h1>
-
-          <label> <b>Username</b> </label>
-          <input type="text" value={this.state.username} onChange={this.onNameChange} required/>
-          <br></br>
-          <label> <b>Password</b> </label>
-          <input type="text" value={this.state.password} onChange={this.onPasswordChange} required/>
-          <br></br>
-          <button type="submit" className="btn">Login</button>
-        </form>
-			</div>
-		)
-	}
-}
-
-render(
-  <Login />,
-	document.getElementById('root')
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/board" component={Board} />
+        <Route component={Notfound} />
+      </Switch>
+    </div>
+  </Router>
 )
+
+
+ReactDOM.render(routing, document.getElementById('root'))
